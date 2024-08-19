04d18650ad42ce7b51d31857a183f6e31cca1846
johnarmandalvarez <aalvarezpcl@gmail.com>
2024-08-19 16:55:39 +0800

    Create MovieQuoteInfo.java

---
+++ Chapter1/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo{
+
+public static void main(String[] args){
+
+System.out.println("Qoute: Its a leap of faith. ”);
+System.out.println("Movie: Spider man into the spider verse. ");
+System.out.println("Actor: jake johnson. ");
+System.out.println("Year: 2018 ");
+
+}
+}
