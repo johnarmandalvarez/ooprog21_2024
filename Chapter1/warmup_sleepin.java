2d46990c2cd106f761e35f79d13c02566faeb1d5
johnarmandalvarez <aalvarezpcl@gmail.com>
2024-09-02 08:50:56 +0800

    Add files via upload

---
+++ Chapter1/warmup_sleepin.java
@@ -0,0 +1,11 @@
+public class warmup_sleepin{
+   public static boolean sleepln(boolean weekday, boolean vacation){
+   return !weekday;
+   }
+   public static void main(String[]args){
+      System.out.println(sleepln(false,false));
+      System.out.println(sleepln(true,false));
+      System.out.println(sleepln(false,true));
+
+   }
+}
