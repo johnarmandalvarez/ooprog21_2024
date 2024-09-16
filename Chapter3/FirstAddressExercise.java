e34036619abe4448faed6b2dab1d61e7eb03d290
johnarmandalvarez <aalvarezpcl@gmail.com>
2024-09-16 19:14:23 +0800

    Create FirstAddressExercise.java

---
+++ Chapter3/FirstAddressExercise.java
@@ -0,0 +1,24 @@
+public class FirstAddressExercise {
+    
+    public static void main(String[] args) {
+        
+        displayAddress();
+    }
+    
+   
+    public static void displayAddress() {
+        String add = "Xinghua East Road";
+        String district = "Chaoyang district";
+        String capital = "Beijing";
+        String zipCode = "100028";
+        String country = "China";
+        String companyName ="Xiaomi Co.";
+
+        System.out.println(add);
+       
+        System.out.println(district + ", " + capital + " " + zipCode);
+        System.out.println(country);
+        
+        System.out.println("Company: " + companyName);
+    }
+}
