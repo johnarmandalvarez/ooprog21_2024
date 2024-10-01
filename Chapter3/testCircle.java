2a7a0b87de5f2ce9c0579fe32b5a353041e570c7
johnarmandalvarez <aalvarezpcl@gmail.com>
2024-10-01 23:49:12 +0800

    Create testCircle.java

---
+++ Chapter3/testCircle.java
@@ -0,0 +1,16 @@
+
+public class TestCircle {
+        public static void main(String[] args) {
+            Circle a = new Circle();
+            a.setRadius(3);
+            System.out.println("Radius of the first circle: " + Circle.getRadius());
+            System.out.println("Diameter of the first circle: " + Circle.getDiameter());
+            System.out.println("Area of the first circle: " + Circle.getArea());
+    
+            Circle b = new Circle();
+            b.setRadius(20);
+            System.out.println("Radius of the second circle: " + Circle.getRadius());
+            System.out.println("Diameter of the second circle: " + Circle.getDiameter());
+            System.out.println("Area of the second circle: " + Circle.getArea());
+        }
+    }
